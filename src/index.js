557526415091887f78229b1bb39a4afce9fe510c
DevMak <sarunimark06@gmail.com>
2025-10-16 07:49:18 +0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import attachmentRoutes from './routes/attachmentRoutes.js';
 import tagRoutes from './routes/tagsRoute.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,12 +20,27 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
+// Configure cors
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://your-frontend-domain.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/folders', folderRoutes);
-app.use('api/attachments', attachmentRoutes);
+app.use('/api/attachments', attachmentRoutes);
 app.use('/api/tags', tagRoutes);
 
 
